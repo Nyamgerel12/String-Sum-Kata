df36cc9234906950a3499e9682263739e6717d64
Nyamgerel12 <zgerlee12@gmail.com>
2026-09-21 21:29:43 +0200

    step2: (Grün): Eine Zahl und leeren String verarbeiten - Nyamgerel

--- src/main/java/StringSum.java
+++ src/main/java/StringSum.java
@@ -1,5 +1,11 @@
 public class StringSum {
     public String sum(String num1, String num2) {
+        if ("".equals(num1) && "".equals(num2)) {
         return "0"; 
         }
+        if (!"".equals(num1)) {
+            return num1;
+        }
+        return num2;
+    }
 }
